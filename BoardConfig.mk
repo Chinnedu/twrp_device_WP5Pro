73cd484734ed7b7a9257f349efa69896c639e5a2
infinixs5lite <kelvinchinedu97@gmail.com>
2023-04-12 15:29:36 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/oukitel/WP5Pro
+DEVICE_PATH := device/oukitel/WP5_Pro
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
