993ddf4135f7ed6077d491335ec49c2b4bd9d279
infinixs5lite <kelvinchinedu97@gmail.com>
2023-04-12 14:21:13 +0100

    Update twrp_WP5_Pro.mk

--- twrp_WP5_Pro.mk
+++ twrp_WP5_Pro.mk
@@ -7,16 +7,13 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Inherit from WP5_Pro device
-$(call inherit-product, device/tecno/WP5_Pro/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from WP5_Pro device
+$(call inherit-product, device/tecno/WP5_Pro/device.mk)
 
 PRODUCT_DEVICE := WP5_Pro
 PRODUCT_NAME := twrp_WP5_Pro
